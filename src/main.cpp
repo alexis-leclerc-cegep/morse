7cc29a6aa7649c659702a2a65a0e874f2d175f3d
alexis-leclerc-cegep <1942518@cegeprdl.ca>
2022-02-06 19:54:36 -0500

    bit of cleanin up

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 #include "morse.h"
 
 const int LED_PIN = 23;
-const int PULSE_LENGTH = 50;
+const int PULSE_LENGTH = 20;
 char* serialString();
-MyMorse morse(50, LED_PIN);
+MyMorse morse(PULSE_LENGTH, LED_PIN);
 String readString;
 
 void setup() {
@@ -15,7 +15,7 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Morse2Led");
   Serial.println("Programmed by Addison Sears-Collins");
-  Serial.println("Copied and adapted by Alexis Leclerc");
+  Serial.println("Copied and adapted to OOP by Alexis Leclerc");
   Serial.println("");
   Serial.print("Enter you message : ");
 }
@@ -39,62 +39,3 @@ void loop() {
     }
   }
 }
-
-
-
-
-
-
-  /*
-  while(Serial.available()){
-    delay(1);
-    char c = Serial.read();
-    if (c == 'a'){break;}
-    Serial.print(c);
-    readString += c;
-  }
-  if (readString.length() > 0)
-  {
-      Serial.println(readString);
-      Serial.println("yo");
-      readString = "";
-  }
-  
-  
-}
-  static boolean needPrompt=true;
-    char* userInput;
-    if (needPrompt)
-    {
-      Serial.println("Entrez la phrase : ");
-      needPrompt=false;
-    }
-    userInput = serialString();
-    if (userInput!=NULL)
-    {
-      morse.flash_string("..-");
-      needPrompt=true;
-    }
-}
-
-
-char* serialString()
-{
-  static char str[21]; // For strings of max length=20
-  if (!Serial.available()) return NULL;
-  delay(64); // wait for all characters to arrive
-  memset(str,0,sizeof(str)); // clear str
-  byte count=0;
-  while (Serial.available())
-  {
-    char c=Serial.read();
-    if (c>=32 && count<sizeof(str)-1)
-    {
-      str[count]=c;
-      count++;
-    }
-  }
-  str[count]='\0'; // make it a zero terminated string
-  return str;
-}
-*/
